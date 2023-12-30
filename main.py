bf25ffcb6a76d211e4caf6a6b3c2a4730f68eff6
sirococoa <torakia.mcml178@gmail.com>
2023-12-30 10:37:35 +0900

    Add Line class

--- main.py
+++ main.py
@@ -12,10 +12,19 @@ class Point:
     def draw(self):
         pyxel.circb(self.x, self.y, self.r, 7)
 
+class Line:
+    def __init__(self, s :Point, t :Point) -> None:
+        self.s = s
+        self.t = t
+    
+    def draw(self):
+        pyxel.line(self.s.x, self.s.y, self.t.x, self.t.y, 7)
+
 class App:
     def __init__(self):
         pyxel.init(WINSOW_W, WINSOW_H)
         self.points = [Point(50, 50), Point(50, 150), Point(150, 150), Point(150, 50)]
+        self.lines = [Line(*points[:2]), Line(*points[1:3]), Line(*points[2:4])]
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -23,6 +32,8 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
+        for line in self.lines:
+            line.draw()
         for point in self.points:
             point.draw()
 
