8ff6676786c9b079516d1e2fce0511a2befeed1e
sirococoa <torakia.mcml178@gmail.com>
2023-12-30 14:01:16 +0900

    Debug Only use T1

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ class Transformer:
         scale = length(baseVector) / length(targetVector)
         S = [[scale, 0, 0], [0, scale, 0], [0, 0, 1]] # Scale matrix to scale base -> target
         T2 = [[1, 0, target.s.x], [0, 1, -target.s.y], [0, 0, 1]] # Translation matrix to move (0, 0) -> target
-        T = product_matrix(T2, product_matrix(S, product_matrix(R, T1)))
+        # T = product_matrix(T2, product_matrix(S, product_matrix(R, T1)))
+        T = T1
         new_lines = []
         for line in self.lines:
             new_sv = product(T, line.s.vector())
